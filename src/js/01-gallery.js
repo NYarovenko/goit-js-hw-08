fe2cc0c7fc03aae6519b2399ae22de1d16d0116b
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-04-24 00:27:08 +0300

    01_gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log('hi');
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const ulContainer = document.querySelector('.gallery');
+
+let liItem = '';
+galleryItems.map(({ preview, original, description }) => {
+  liItem += `
+        <li class="gallery__item">
+             <a class="gallery__link" href="${original}">
+                 <img class="gallery__image" src="${preview}" alt="${description}"/>
+              </a>
+        </li>
+    `;
+});
+ulContainer.innerHTML = liItem;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
