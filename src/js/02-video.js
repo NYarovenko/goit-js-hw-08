b9af75cd03b89090dd13820d14fc88ee7f7dca95
NYarovenko <NYarovenko.Full.Stack@gmail.com>
2023-04-25 00:56:58 +0300

    02_video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Vimeo from '@vimeo/player';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+startPlayer();
+
+function stopTime() {
+  return localStorage.getItem(LOCALSTORAGE_KEY) || '';
+}
+
+function startPlayer() {
+  player
+    .setCurrentTime(parseInt(stopTime()))
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
+
+const throttle = require('lodash.throttle');
+
+const throttled = throttle(tracking, 1000, { trailing: false });
+player.on('timeupdate', throttled);
+
+function tracking({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+}
